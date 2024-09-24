2d4b159950e6e23c64b740fe272107a8c2e80368
Dorian <dorian.luyet@netplus.ch>
2024-09-24 23:28:38 +0200

    menu added

--- OhmCalc-cli.py
+++ OhmCalc-cli.py
@@ -13,8 +13,8 @@ class Ohm():
             splat_r = raw_r.split("*")
             mantisse_r = float(splat_r[0])
             exposant = splat_r[1]
-            sfhjdh = exposant.split("^", 1)
-            power = int(sfhjdh[1])
+            exp_split = exposant.split("^", 1)
+            power = int(exp_split[1])
             exposant = 10**power
             r = mantisse_r*exposant
         else: 
@@ -24,8 +24,8 @@ class Ohm():
             splat_i = raw_i.split("*")
             mantisse_i = float(splat_i[0])
             exposant = splat_i[1]
-            sfhjdh = exposant.split("^", 1)
-            power = int(sfhjdh[1])
+            exp_split = exposant.split("^", 1)
+            power = int(exp_split[1])
             exposant = 10**power
             i = mantisse_i*exposant
         else:
@@ -41,8 +41,8 @@ class Ohm():
             splat_r = raw_r.split("*")
             mantisse_r = float(splat_r[0])
             exposant = splat_r[1]
-            sfhjdh = exposant.split("^", 1)
-            power = int(sfhjdh[1])
+            exp_split = exposant.split("^", 1)
+            power = int(exp_split[1])
             exposant = 10**power
             r = mantisse_r*exposant
         else: 
@@ -51,8 +51,8 @@ class Ohm():
             splat_u = raw_u.split("*")
             mantisse_u = float(splat_u[0])
             exposant = splat_u[1]
-            sfhjdh = exposant.split("^", 1)
-            power = int(sfhjdh[1])
+            exp_split = exposant.split("^", 1)
+            power = int(exp_split[1])
             exposant = 10**power
             u = mantisse_u*exposant
         else: 
@@ -68,8 +68,8 @@ class Ohm():
             splat_u = raw_u.split("*")
             mantisse_u = float(splat_u[0])
             exposant = splat_u[1]
-            sfhjdh = exposant.split("^", 1)
-            power = int(sfhjdh[1])
+            exp_split = exposant.split("^", 1)
+            power = int(exp_split[1])
             exposant = 10**power
             u = mantisse_u*exposant
         else: 
@@ -79,8 +79,8 @@ class Ohm():
             splat_i = raw_i.split("*")
             mantisse_i = float(splat_i[0])
             exposant = splat_i[1]
-            sfhjdh = exposant.split("^", 1)
-            power = int(sfhjdh[1])
+            exp_split = exposant.split("^", 1)
+            power = int(exp_split[1])
             exposant = 10**power
             i = mantisse_i*exposant
         else:
@@ -89,4 +89,30 @@ class Ohm():
         r = float(u/i)
         print(r, "Ω")
 
+def afficher_menu():
+    menu = "===============================================\n"
+    menu += "             Choose your formula\n"
+    menu += "-----------------------------------------------\n"
+    menu += " U - tension\n"
+    menu += " R - resistence\n"
+    menu += " I - Current\n"
+    menu += "...\n"
+    menu += ' Q - Quit\n'
+    menu += "===============================================\n"
+    print(menu)
 
+def ask_user_choice(question:str):
+    choix = input(question)
+    try:
+        lettre_choix = choix
+    except:
+        pass
+    return lettre_choix
+
+def main():
+    if_fin_boucle = False
+    while not is_fin_boucle:
+        os.system('cls')
+        afficher_menu()
+        choix_usr = ask_user_choice("Your choice : ")
+        print()
